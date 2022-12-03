e00c5949e93f2ec6f45687f548fcdd3a4204506c
Hathaway-Shan <hathaway.shan@gmail.com>
2022-12-03 13:22:13 -0800

    took a crack at getting a display working did not go well

---
+++ client/components/MainPage/MainPage.js
@@ -0,0 +1,17 @@
+import jobCard from '../jobCard/jobCard.js';
+import useJobs from '../../hooks/useJobs.js';
+
+export default function MainPage() {
+  const { jobs, setJobs, loading, error } = useJobs();
+  return (
+    <main>
+      <span>{loading ? <>loading...</> : <></>}</span>
+      <span>{error ? <h2>{error}</h2> : <></>}</span>
+      <div className="jobWrapper">
+        {jobs.map((job) => (
+          <jobCard key={job.id} {...job} />
+        ))}
+      </div>
+    </main>
+  );
+}
---
+++ client/services/job-utils.js
@@ -0,0 +1,16 @@
+const BASE_URL = 'http://localhost:7891/api/v1';
+
+export async function getAllJobs() {
+  const response = await fetch(`${BASE_URL}/jobs`, {
+    method: ' GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+  if (response.ok) {
+    const jobList = await response.json();
+    return jobList;
+  }
+}
