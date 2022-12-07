e8722e14c55a9c5e9c0d9a472cae4f3285a5d460
Hathaway-Shan <hathaway.shan@gmail.com>
2022-12-07 12:18:40 -0800

    fixed issue with some display cards being longer than others

--- client/components/JobList/JobList.jsx
+++ client/components/JobList/JobList.jsx
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import styles from './JobList.module.css';
 import JobCard from '../jobCard/jobCard';
+// import ReactPaginate from 'react-paginate;'
 
 export default function JobList() {
-  //break this out into it's own hook with state and error
+  //break this out into it's own useJobs hook with state and error
   const [jobs, setJobs] = useState([]);
   const [error, setError] = useState('');
+  //loading may eventually need to be broken out into it's own component
   const [loading, setLoading] = useState(true);
+  //
+  const [pageNumber, setPageNumber] = useState(0);
+  const jobsPerPage = 12;
+
+  const pagesVisited = pageNumber * jobsPerPage;
+
+  console.log(pagesVisited + jobsPerPage); //returns 12
+
+  const displayJobs = jobs
+    .slice(pagesVisited, pagesVisited + jobsPerPage)
+    .map((job) => {
+      console.log('DISPLAY JOBS');
+      return (
+        <div className={styles.wrapper}>
+          {jobs && <JobCard key={job.id} {...job} />};
+        </div>
+      );
+    });
 
   useEffect(() => {
     getJobs();
   }, []);
+
   function getJobs() {
     fetch('/api/v1/jobs').then((response) => {
       return response
@@ -38,12 +59,5 @@ export default function JobList() {
     );
   }
 
-  return (
-    <div className={styles.wrapper}>
-      {jobs &&
-        jobs.map((job) => {
-          return <JobCard key={job.id} {...job} />;
-        })}
-    </div>
-  );
+  return <div className={styles.wrapper}>{displayJobs}</div>;
 }
