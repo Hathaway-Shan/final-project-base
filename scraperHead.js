6be6a88df97d737d3d1ff55565cfca18f4552d6f
Hathaway-Shan <hathaway.shan@gmail.com>
2022-12-14 15:18:31 -0800

    changed the name of scraperHead file and script in package json to better reflect what it does

---
+++ scraperHead.js
@@ -0,0 +1,22 @@
+import Job from './server/models/Jobs.js';
+import browserObject from './browser.js';
+import pageScraper from './pageScraper.js';
+
+(async () => {
+  let jobsInserted = 0;
+
+  //Start Browser and create a browser instance
+  let browserInstance = await browserObject.startBrowser();
+  //returns scraped data which we need to capture
+  const scrapedData = await pageScraper.scraper(browserInstance);
+
+  //around here is where we should stuff it into the DB
+  // console.log('SCRAPED DATA IS: ', scrapedData);
+  scrapedData.forEach((element) => {
+    Job.insert(element);
+    jobsInserted++;
+    console.log(`${jobsInserted} job successfully inserted`);
+  });
+
+  console.log(`${jobsInserted} jobs added to DB`);
+})();
