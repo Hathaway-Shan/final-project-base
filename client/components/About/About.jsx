98c26c8c0bbba00f26e39c9614922f592a0d2624
Hathaway-Shan <hathaway.shan@gmail.com>
2023-02-01 18:24:56 -0800

    created basic about component

--- client/components/About/About.jsx
+++ client/components/About/About.jsx
@@ -2,15 +2,40 @@ import React from 'react';
 import styles from './About.module.css';
 
 export default function About() {
-  //
   return (
-    <div class={styles.aboutWrapper}>
+    <div className={styles.aboutWrapper}>
       <div className={styles.row}>
         <div className={styles.column}>
-          <div class={styles.blueColumn}>text in column 1</div>
+          <div className={styles.blueColumn}>text in column 1</div>
         </div>
         <div className={styles.column}>
-          <div class={styles.greenColumn}>text in column 2</div>
+          <img
+            src="/images/programing.png"
+            className={styles.devImage}
+          />
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.column}>
+          <div className={styles.orangeColumn}>
+            text in row 2 column 1
+          </div>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.column}>
+          <div className={styles.purpleRow}>
+            <div className={styles.spacerRow}></div>
+            <img
+              className={styles.aboutImg}
+              src="/images/github.png"
+            />
+            <img
+              className={styles.aboutImg}
+              src="/images/linkedin.png"
+            />
+            <img className={styles.aboutImg} src="/images/Logo.png" />
+          </div>
         </div>
       </div>
     </div>
